343e821d3791f2afb35c1a2680271a1c65f2a289
mbarrins <maija.barrins@gmail.com>
2019-05-13 23:37:17 +0100

    added count_elements method

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,16 @@
 def count_elements(array)
-  # code goes here
+	# new_hash = {}
+  # array.uniq.each do |x|
+  #   new_hash[x] = array.count(x)
+  # end
+  # new_hash
+
+  # array.uniq.map{|x| [x, array.count(x)]}.to_h
+
+ 	new_hash = {}
+ 	array.each do |x|
+ 		new_hash.include?(x) ? new_hash[x] += 1 : new_hash[x] = 1
+ 	end
+ 	new_hash
 end
  
